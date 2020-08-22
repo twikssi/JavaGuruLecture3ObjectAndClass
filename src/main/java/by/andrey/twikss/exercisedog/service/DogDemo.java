d279fd14b33434c0f72ca718f69b79aafadcca13
twikssi <limpbiski15@gmail.com>
2020-08-22 19:26:29 +0300

    created method bark

---
+++ src/main/java/by/andrey/twikss/exercisedog/service/DogDemo.java
@@ -0,0 +1,20 @@
+package by.andrey.twikss.exercisedog.service;
+
+import by.andrey.twikss.exercisedog.bean.Dog;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class DogDemo {
+    private Logger logger = LoggerFactory.getLogger(DogDemo.class);
+    private Dog dog;
+
+    public DogDemo(Dog dog) {
+        this.dog = dog;
+    }
+
+        public String bark(){
+        String dogBark = "Dog " + dog.getName() + " say: bau bau bau";
+        logger.info(dogBark);
+        return dogBark;
+    }
+}
