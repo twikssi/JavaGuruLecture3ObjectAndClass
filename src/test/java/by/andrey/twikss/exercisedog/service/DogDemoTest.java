e5cb22c2911b0de017333bdfc4afea85c17a4a49
twikssi <limpbiski15@gmail.com>
2020-08-22 19:27:20 +0300

    improved test for method Bark

---
+++ src/test/java/by/andrey/twikss/exercisedog/service/DogDemoTest.java
@@ -0,0 +1,21 @@
+package by.andrey.twikss.exercisedog.service;
+
+import by.andrey.twikss.exercisedog.bean.Dog;
+import junit.framework.TestCase;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DogDemoTest extends TestCase {
+
+    @Test
+    public void testIfDogBarkShouldReturnTrue (){
+        Dog dog = new Dog(4,"red","kloka");
+        DogDemo dogDemo = new DogDemo(dog);
+
+        boolean expected = true;
+        boolean actual = dogDemo.bark().equals("Dog kloka say: bau bau bau");
+
+        assertEquals(expected, actual);
+    }
+
+}
