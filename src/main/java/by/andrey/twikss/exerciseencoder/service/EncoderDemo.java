b089acc578f3d38f531f14dd7cd00aeda7195ce4
twikssi <limpbiski15@gmail.com>
2020-08-23 15:27:51 +0300

    deleted import and added private construcror in class EncoderDemo

--- src/main/java/by/andrey/twikss/exerciseencoder/service/EncoderDemo.java
+++ src/main/java/by/andrey/twikss/exerciseencoder/service/EncoderDemo.java
@@ -1,12 +1,14 @@
 package by.andrey.twikss.exerciseencoder.service;
 
-import by.andrey.twikss.exercisedog.service.DogDemo;
 import by.andrey.twikss.exerciseencoder.bean.Encoder;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
 public class EncoderDemo {
     private static Logger logger = LoggerFactory.getLogger(EncoderDemo.class);
+
+    private EncoderDemo() {
+    }
 
     public static char encoder(Encoder encoder) {
         char encoderNumberIs = (char) encoder.getNumber();
